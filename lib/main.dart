29c76e6851490e0ff8de81cbc3fef868993ffddf
ellenoireQ <ketchupnaiso@gmail.com>
2025-09-02 10:04:15 +0700

    Weathe: set blur radius to 20

--- lib/main.dart
+++ lib/main.dart
@@ -112,7 +112,7 @@ class _WeatherAppBar extends State<WeatherAppBar> {
             boxShadow: [
               BoxShadow(
                 color: Colors.blue.withOpacity(0.6),
-                blurRadius: 60,
+                blurRadius: 20,
                 spreadRadius: 1,
                 offset: Offset(0, 0),
               ),
