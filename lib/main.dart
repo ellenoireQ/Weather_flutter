4cddfe1f9ca78d6ec16bf218cadcd6fa77819301
ellenoireQ <ketchupnaiso@gmail.com>
2025-09-04 11:15:08 +0700

    Weather: code: remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
-import 'dart:io';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
